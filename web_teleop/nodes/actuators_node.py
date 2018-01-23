713b83f63e357bab2b79da8c1cf95ff8b3d9bf03
Sarang Joshi <sarangj@cs.washington.edu>
2018-01-22 18:01:29 -0800

    Starting lab 10, almost done with piping arm movements

--- web_teleop/nodes/actuators_node.py
+++ web_teleop/nodes/actuators_node.py
@@ -3,6 +3,7 @@
 import fetch_api
 import rospy
 from web_teleop.srv import SetTorso, SetTorsoResponse
+from web_teleop.srv import SetHeadPanTilt, SetHeadPanTiltResponse
 
 
 def wait_for_time():
@@ -15,11 +16,20 @@ def wait_for_time():
 class ActuatorServer(object):
     def __init__(self):
         self._torso = fetch_api.Torso()
+        self._head = fetch_api.Head()
 
     def handle_set_torso(self, request):
-        # TODO: move the torso to the requested height
         self._torso.set_height(request.height)
         return SetTorsoResponse()
+
+    def handle_set_head_pan_tilt(self, request):
+        self._head.pan_tilt(request.pan, request.tilt)
+        return SetHeadPanTiltResponse()
+
+    def handle_set_arm(self, request):
+        arm = fetch_api.Arm()
+        arm.move_to_joints(fetch_api.ArmJoints.from_list([request.shoulder_pan, request.shoulder_lift, request.upperarm_roll, request.elbow_flex, request.forearm_roll, request.wrist_flex, request.wrist_roll]))
+        return SetArmResponse()
 
 
 def main():
@@ -28,6 +38,8 @@ def main():
     server = ActuatorServer()
     torso_service = rospy.Service('web_teleop/set_torso', SetTorso,
                                   server.handle_set_torso)
+    head_service = rospy.Service('web_teleop/set_head_pan_tilt', SetHeadPanTilt,
+                                  server.handle_set_head_pan_tilt)
     rospy.spin()
 
 
