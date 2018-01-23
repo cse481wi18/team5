f44eabafd146559d44caee97ed36c48b54f7f123
Sarang Joshi <sarangj@cs.washington.edu>
2018-01-23 03:40:16 -0800

    Finished labs 9-10 for blog 3

--- web_teleop/nodes/actuators_node.py
+++ web_teleop/nodes/actuators_node.py
@@ -4,6 +4,8 @@ import fetch_api
 import rospy
 from web_teleop.srv import SetTorso, SetTorsoResponse
 from web_teleop.srv import SetHeadPanTilt, SetHeadPanTiltResponse
+from web_teleop.srv import SetArm, SetArmResponse
+from web_teleop.srv import SetGripper, SetGripperResponse
 
 
 def wait_for_time():
@@ -17,6 +19,8 @@ class ActuatorServer(object):
     def __init__(self):
         self._torso = fetch_api.Torso()
         self._head = fetch_api.Head()
+        self._arm = fetch_api.Arm()
+        self._gripper = fetch_api.Gripper()
 
     def handle_set_torso(self, request):
         self._torso.set_height(request.height)
@@ -27,10 +31,12 @@ class ActuatorServer(object):
         return SetHeadPanTiltResponse()
 
     def handle_set_arm(self, request):
-        arm = fetch_api.Arm()
-        arm.move_to_joints(fetch_api.ArmJoints.from_list([request.shoulder_pan, request.shoulder_lift, request.upperarm_roll, request.elbow_flex, request.forearm_roll, request.wrist_flex, request.wrist_roll]))
+        self._arm.move_to_joints(fetch_api.ArmJoints.from_list([request.shoulder_pan, request.shoulder_lift, request.upperarm_roll, request.elbow_flex, request.forearm_roll, request.wrist_flex, request.wrist_roll]))
         return SetArmResponse()
 
+    def handle_set_gripper(self, request):
+        self._gripper.close()
+        return SetGripperResponse()
 
 def main():
     rospy.init_node('web_teleop_actuators')
@@ -40,6 +46,10 @@ def main():
                                   server.handle_set_torso)
     head_service = rospy.Service('web_teleop/set_head_pan_tilt', SetHeadPanTilt,
                                   server.handle_set_head_pan_tilt)
+    arm_service = rospy.Service('web_teleop/set_arm', SetArm,
+                                  server.handle_set_arm)
+    gripper_service = rospy.Service('web_teleop/set_gripper', SetGripper,
+                                  server.handle_set_gripper)
     rospy.spin()
 
 
