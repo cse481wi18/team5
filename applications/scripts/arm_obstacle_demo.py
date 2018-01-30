2f7716ee13a57e281a495a57f8c89ac498db080f
CSE 481C Team 5 <cse481c5@gmail.com>
2018-01-30 14:12:46 -0800

    Abhishek Finished Lab 22

--- applications/scripts/arm_obstacle_demo.py
+++ applications/scripts/arm_obstacle_demo.py
@@ -4,6 +4,9 @@ import fetch_api
 import rospy
 from moveit_python import PlanningSceneInterface
 from geometry_msgs.msg import PoseStamped
+from moveit_msgs.msg import OrientationConstraint
+
+RUN_WITH_OC_CONSTRAINT = True # RUN with orientation constraint if True else Don't
 
 def wait_for_time():
     """Wait for simulated time to begin.
@@ -25,34 +28,52 @@ def main():
     table_x = 0.8
     table_y = 0
     table_z = 0.4
+    if (RUN_WITH_OC_CONSTRAINT):
+        table_z = 0.6   
+        
     planning_scene.addBox('table', table_size_x, table_size_y, table_size_z,
                           table_x, table_y, table_z)
 
     # Create divider obstacle
-    planning_scene.removeCollisionObject('divider')
-    size_x = 0.3 
-    size_y = 0.01
-    size_z = 0.4 
-    x = table_x - (table_size_x / 2) + (size_x / 2)
-    y = 0 
-    z = table_z + (table_size_z / 2) + (size_z / 2)
-    planning_scene.addBox('divider', size_x, size_y, size_z, x, y, z)
+    if (not RUN_WITH_OC_CONSTRAINT):
+        planning_scene.removeCollisionObject('divider')
+        size_x = 0.3 
+        size_y = 0.01
+        size_z = 0.4 
+        x = table_x - (table_size_x / 2) + (size_x / 2)
+        y = 0 
+        z = table_z + (table_size_z / 2) + (size_z / 2)
+        planning_scene.addBox('divider', size_x, size_y, size_z, x, y, z)
 
     pose1 = PoseStamped()
     pose1.header.frame_id = 'base_link'
-    pose1.pose.position.x = 0.7 # 0.7 is pretty reliable
+    pose1.pose.position.x = 0.6 # 0.7 is pretty reliable
+    if (RUN_WITH_OC_CONSTRAINT):
+        pose1.pose.position.x = 0.5
     pose1.pose.position.y = -0.3
     pose1.pose.position.z = 0.75
     pose1.pose.orientation.w = 1
 
     pose2 = PoseStamped()
     pose2.header.frame_id = 'base_link'
-    pose2.pose.position.x = 0.7 # 0.7 is pretty reliable
+    pose2.pose.position.x = 0.6 # 0.7 is pretty reliable
+    if (RUN_WITH_OC_CONSTRAINT):
+        pose2.pose.position.x = 0.5
     pose2.pose.position.y = 0.3
     pose2.pose.position.z = 0.75
     pose2.pose.orientation.w = 1
 
     arm = fetch_api.Arm()
+
+    oc = OrientationConstraint()
+    oc.header.frame_id = 'base_link'
+    oc.link_name = 'wrist_roll_link'
+    oc.orientation.w = 1
+    oc.absolute_x_axis_tolerance = 0.1
+    oc.absolute_y_axis_tolerance = 0.1
+    oc.absolute_z_axis_tolerance = 3.14
+    oc.weight = 1.0
+
     def shutdown():
         arm.cancel_all_goals()
     rospy.on_shutdown(shutdown)
@@ -61,22 +82,28 @@ def main():
         'allowed_planning_time': 15,
         'execution_timeout': 10,
         'num_planning_attempts': 5,
-        'replan': False
+        'replan': False,
     }
-    planning_scene.removeAttachedObject('tray')
+
+    if (RUN_WITH_OC_CONSTRAINT):
+        kwargs['orientation_constraint'] = oc
+    else:
+        planning_scene.removeAttachedObject('tray')
+
     error = arm.move_to_pose(pose1, **kwargs)
     if error is not None:
         rospy.logerr('Pose 1 failed: {}'.format(error))
     else:
         rospy.loginfo('Pose 1 succeeded')
-        frame_attached_to = 'gripper_link'
-        frames_okay_to_collide_with = [
-            'gripper_link', 'l_gripper_finger_link', 'r_gripper_finger_link'
-        ]
-        planning_scene.attachBox('tray', 0.3, 0.07, 0.01, 0.05, 0, 0,
-                                 frame_attached_to, frames_okay_to_collide_with)
-        planning_scene.setColor('tray', 1, 0, 1)
-        planning_scene.sendColors()
+        if (not RUN_WITH_OC_CONSTRAINT):
+            frame_attached_to = 'gripper_link'
+            frames_okay_to_collide_with = [
+                'gripper_link', 'l_gripper_finger_link', 'r_gripper_finger_link'
+            ]
+            planning_scene.attachBox('tray', 0.3, 0.07, 0.01, 0.05, 0, 0,
+                                     frame_attached_to, frames_okay_to_collide_with)
+            planning_scene.setColor('tray', 1, 0, 1)
+            planning_scene.sendColors()
     rospy.sleep(1)
     error = arm.move_to_pose(pose2, **kwargs)
     if error is not None:
@@ -86,7 +113,8 @@ def main():
 
     planning_scene.removeCollisionObject('table')
     planning_scene.removeCollisionObject('divider')
-    planning_scene.removeAttachedObject('tray')
+    if (not RUN_WITH_OC_CONSTRAINT):
+        planning_scene.removeAttachedObject('tray')
 
     
 
--- fetch_api/src/fetch_api/arm.py
+++ fetch_api/src/fetch_api/arm.py
@@ -81,7 +81,8 @@ class Arm(object):
                      plan_only=False,
                      replan=False,
                      replan_attempts=5,
-                     tolerance=0.01):
+                     tolerance=0.01,
+                     orientation_constraint=None):
         """Moves the end-effector to a pose, using motion planning.
 
         Args:
@@ -115,6 +116,8 @@ class Arm(object):
         goal_builder.replan = replan
         goal_builder.replan_attempts = replan_attempts
         goal_builder.tolerance = tolerance
+        if (orientation_constraint != None):
+            goal_builder.add_path_orientation_constraint(orientation_constraint)
         goal = goal_builder.build()
     
         self.move_group_client.send_goal(goal)
