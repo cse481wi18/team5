2cef1f9e8caf2d7cb9f32c6087052d74b25abd61
CSE 481C Team 5 <cse481c5@gmail.com>
2018-01-31 16:47:24 -0800

    Sai finished lab 23

--- applications/scripts/ee_pose_demo.py
+++ applications/scripts/ee_pose_demo.py
@@ -5,11 +5,18 @@ import math
 import tf
 import geometry_msgs.msg
 
+def wait_for_time():
+    """Wait for simulated time to begin.
+    """
+    while rospy.Time().now().to_sec() == 0:
+        pass
 
 def main():
+    print "lol"
     rospy.init_node("ee_demo")
     wait_for_time()
     listener = tf.TransformListener()
+    rate = rospy.Rate(1.0)
     """
     Read from frames:
        l_gripper_finger_link
@@ -17,8 +24,14 @@ def main():
        gripper_link
        wrist_roll_link
     """
-    while True:
-        rospy.sleep(1000)
+    while not rospy.is_shutdown():
+        try:
+            (trans, rot) = listener.lookupTransform('gripper_link', 'base_link', rospy.Time(0))
+            rospy.loginfo("%s %s", trans, rot)
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            continue
+        rate.sleep()
+
 if __name__ == '__main__':
-    main
+    main()
 
