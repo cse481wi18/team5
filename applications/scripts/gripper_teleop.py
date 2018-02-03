5195a37b7b558f4ff93d8b313c5523ce51e9f71d
CSE 481C Team 5 <cse481c5@gmail.com>
2018-02-02 23:15:20 -0800

    Sai: made the interactive marker show up on the gripper

--- applications/scripts/gripper_teleop.py
+++ applications/scripts/gripper_teleop.py
@@ -1,39 +1,67 @@
-#! usr/bin/bash
+#! /usr/bin/env python
 
 import fetch_api
 import rospy
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
 from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback, Marker
+from geometry_msgs.msg import PoseStamped, Point
+import copy
 
 GRIPPER_MESH = 'package://fetch_description/meshes/gripper_link.dae'
 L_FINGER_MESH = 'package://fetch_description/meshes/l_gripper_finger_link.STL'
 R_FINGER_MESH = 'package://fetch_description/meshes/r_gripper_finger_link.STL'
+
+def wait_for_time():
+    while rospy.Time.now().to_sec() == 0:
+        pass
 
 class GripperTeleop(object):
     def __init__(self, arm, gripper, im_server):
         self._arm = arm
         self._gripper = gripper
         self._im_server = im_server
+        pose_st = PoseStamped()
+        pose_st.header.frame_id = "wrist_roll_link"
+        pose_st.pose.position = Point(0, 0, 0)
+        pose_st.pose.orientation.w = 1
+        self._gripper_im = self.get_marker(pose_st)
 
-    def get_marker(pose):
+    def get_marker(self, pose_st):
         gripper_m = Marker()
-        gripper_m.type = marker.mesh_resource
+        gripper_m.header = pose_st.header
+        gripper_m.type = Marker.MESH_RESOURCE
         gripper_m.mesh_resource = GRIPPER_MESH
-        gripper_m.pose = pose
+        gripper_m.pose = copy.deepcopy(pose_st.pose)
+        gripper_m.pose.position.x += 0.166
+        gripper_m.scale.x = 1.0
+        gripper_m.scale.y = 1.0
+        gripper_m.scale.z = 1.0
 
         finger_left_m = Marker()
-        finger_left_m.type = marker.mesh_resource
+        finger_left_m.header = pose_st.header
+        finger_left_m.type = Marker.MESH_RESOURCE
         finger_left_m.mesh_resource = L_FINGER_MESH
-        finger_left_m.pose = pose
+        finger_left_m.pose = copy.deepcopy(pose_st.pose)
+        finger_left_m.pose.position.x += 0.166
+        finger_left_m.pose.position.y -= 0.053
+        finger_left_m.scale.x = 1.0
+        finger_left_m.scale.y = 1.0
+        finger_left_m.scale.z = 1.0
 
         finger_right_m = Marker()
-        finger_right_m.type = marker.mesh_resource
+        finger_right_m.header = pose_st.header
+        finger_right_m.type = Marker.MESH_RESOURCE
         finger_right_m.mesh_resource = R_FINGER_MESH
-        finger_right_m.pose = pose
+        finger_right_m.pose = copy.deepcopy(pose_st.pose)
+        finger_right_m.pose.position.x += 0.166
+        finger_right_m.pose.position.y += 0.053
+        finger_right_m.scale.x = 1.0
+        finger_right_m.scale.y = 1.0
+        finger_right_m.scale.z = 1.0
 
         gripper_control = InteractiveMarkerControl()
         gripper_control.interaction_mode = InteractiveMarkerControl.MOVE_ROTATE
-        gripper_control.orientation = pose.orientation
+        gripper_control.orientation = pose_st.pose.orientation
         gripper_control.always_visible = True
         gripper_control.markers.append(gripper_m)
         gripper_control.markers.append(finger_left_m)
@@ -45,13 +73,20 @@ class GripperTeleop(object):
         gripper_im.description = "Gripper Interactive Marker"
         gripper_im.controls.append(gripper_control)
 
+        for m in gripper_control.markers:
+            m.color.r = 0.0
+            m.color.g = 0.5
+            m.color.b = 0.5
+            m.color.a = 1.0
+
         return gripper_im
 
     def start(self):
-        self._im_server.insert(gripper_im, feedback_cb=self.handle_feedback)
+        self._im_server.insert(self._gripper_im, feedback_cb=self.handle_feedback)
+        self._im_server.applyChanges()
 
     def handle_feedback(self, feedback):
-        pass
+        rospy.loginfo("gripper clicked")
 
 
 class AutoPickTeleop(object):
@@ -61,7 +96,7 @@ class AutoPickTeleop(object):
         self._im_server = im_server
 
     def start(self):
-        # obj_im = InteractiveMarker() ...
+        obj_im = InteractiveMarker()
         self._im_server.insert(obj_im, feedback_cb=self.handle_feedback)
 
     def handle_feedback(self, feedback):
@@ -79,10 +114,13 @@ def main():
     arm = fetch_api.Arm()
 
     im_server = InteractiveMarkerServer('gripper_im_server')
-    auto_pick_im_server = InteractiveMarkerServer('auto_pick_im_server')
+    #auto_pick_im_server = InteractiveMarkerServer('auto_pick_im_server')
     teleop = GripperTeleop(arm, gripper, im_server)
-    auto_pick = AutoPickTeleop(arm, gripper, auto_pick_im_server)
+    #auto_pick = AutoPickTeleop(arm, gripper, auto_pick_im_server)
     teleop.start()
-    auto_pick.start()
+    #auto_pick.start()
 
     rospy.spin()
+
+if __name__=="__main__":
+    main()
