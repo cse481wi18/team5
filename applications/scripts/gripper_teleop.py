26d6048cf8d28cff407607686fb60bb0d4705260
CSE 481C Team 5 <cse481c5@gmail.com>
2018-02-03 01:50:43 -0800

    Sai: mostly done with lab 26 code; just have to implement color change and debug pose movement

--- applications/scripts/gripper_teleop.py
+++ applications/scripts/gripper_teleop.py
@@ -3,13 +3,19 @@
 import fetch_api
 import rospy
 from interactive_markers.interactive_marker_server import InteractiveMarkerServer
-from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback, Marker
+from visualization_msgs.msg import InteractiveMarker, InteractiveMarkerControl, InteractiveMarkerFeedback, Marker, MenuEntry
 from geometry_msgs.msg import PoseStamped, Point
 import copy
 
 GRIPPER_MESH = 'package://fetch_description/meshes/gripper_link.dae'
 L_FINGER_MESH = 'package://fetch_description/meshes/l_gripper_finger_link.STL'
 R_FINGER_MESH = 'package://fetch_description/meshes/r_gripper_finger_link.STL'
+
+
+# Menu entry ids
+GOTO_ENTRY = 1
+OPEN_ENTRY = 2
+CLOSE_ENTRY = 3
 
 def wait_for_time():
     while rospy.Time.now().to_sec() == 0:
@@ -53,6 +59,32 @@ def make_6dof_controls():
     controls.append(copy.deepcopy(control))
     
     return controls
+
+def create_menu():
+    menu_entries = []
+
+    goto_pose_entry = MenuEntry()
+    open_gripper_entry = MenuEntry()
+    close_gripper_entry = MenuEntry()
+    # go to pose
+    goto_pose_entry.id = GOTO_ENTRY
+    goto_pose_entry.command_type = MenuEntry.FEEDBACK
+    goto_pose_entry.title = "Go to pose"
+    menu_entries.append(goto_pose_entry)
+    
+    # open gripper
+    open_gripper_entry.id = OPEN_ENTRY
+    open_gripper_entry.command_type = MenuEntry.FEEDBACK
+    open_gripper_entry.title = "Open gripper"
+    menu_entries.append(open_gripper_entry)
+    
+    # close gripper
+    close_gripper_entry.id = CLOSE_ENTRY
+    close_gripper_entry.command_type = MenuEntry.FEEDBACK
+    close_gripper_entry.title = "Close gripper"
+    menu_entries.append(close_gripper_entry)
+
+    return menu_entries
 
 class GripperTeleop(object):
     def __init__(self, arm, gripper, im_server):
@@ -99,13 +131,14 @@ class GripperTeleop(object):
         finger_right_m.scale.z = 1.0
 
         gripper_control = InteractiveMarkerControl()
-        gripper_control.interaction_mode = InteractiveMarkerControl.MOVE_ROTATE
+        gripper_control.interaction_mode = InteractiveMarkerControl.MENU
         gripper_control.orientation = pose_st.pose.orientation
         gripper_control.always_visible = True
         gripper_control.markers.append(gripper_m)
         gripper_control.markers.append(finger_left_m)
         gripper_control.markers.append(finger_right_m)
-
+        
+        # set every marker in the gripper IM to green
         for m in gripper_control.markers:
             m.color.r = 0.0
             m.color.g = 0.5
@@ -113,12 +146,13 @@ class GripperTeleop(object):
             m.color.a = 1.0
 
         gripper_im = InteractiveMarker()
-        gripper_im.header.frame_id = "wrist_roll_link"
+        gripper_im.header = copy.deepcopy(pose_st.header)
         gripper_im.name = "GripperMarker"
         gripper_im.description = "Gripper Interactive Marker"
         gripper_im.controls.append(gripper_control)
         gripper_im.controls.extend(make_6dof_controls())
         gripper_im.scale = 0.2
+        gripper_im.menu_entries.extend(create_menu())
         return gripper_im
 
     def start(self):
@@ -126,7 +160,26 @@ class GripperTeleop(object):
         self._im_server.applyChanges()
 
     def handle_feedback(self, feedback):
-        rospy.loginfo("gripper clicked")
+        new_pose_st = PoseStamped()
+        new_pose_st.pose = feedback.pose
+        new_pose_st.header = feedback.header
+        if feedback.event_type == InteractiveMarkerFeedback.MENU_SELECT:
+            if feedback.menu_entry_id == GOTO_ENTRY:
+                self._arm.move_to_pose(new_pose_st)
+            elif feedback.menu_entry_id == OPEN_ENTRY:
+                self._gripper.open()
+            elif feedback.menu_entry_id == CLOSE_ENTRY:
+                self._gripper.close(self._gripper.MAX_EFFORT)
+            else:
+                pass
+        elif feedback.event_type == InteractiveMarkerFeedback.POSE_UPDATE:
+            # update color of the gripper based on if pose is possible
+            if self._arm.compute_ik(new_pose_st):
+                print("valid movement")
+            else:
+                print("invalid movement")
+        # Does insert on self._im_server need to be called again
+        self._im_server.applyChanges()
 
 
 class AutoPickTeleop(object):
