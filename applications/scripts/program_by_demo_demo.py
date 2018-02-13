c89d22855e8bd8470b978ae91931835f10cf2a31
CSE 481C Team 5 <cse481c5@gmail.com>
2018-02-13 01:47:32 -0800

    Progress on 29

--- applications/scripts/program_by_demo_demo.py
+++ applications/scripts/program_by_demo_demo.py
@@ -5,14 +5,17 @@ import copy
 
 import rospy
 from ar_track_alvar_msgs.msg import AlvarMarkers
-from robot_controllers_msgs.msg import QueryControllerStatesGoal, ControllerState
+from robot_controllers_msgs.msg import QueryControllerStatesGoal, ControllerState, QueryControllerStatesAction
+import actionlib
+import fetch_api
+from joint_state_reader import JointStateReader
 
 FILE_NAME = "program_by_demo_saved.pickle"
 
-HELP_MESSAGE = """ This program lets you save a list of actions the gripper can take
+HELP_MESSAGE = """ This program lets you save a list of poses the gripper can take
 for future use. These commands do not manipulate the robot.
 Commands:
-    start: If a programmed action is not currently being created, start creation of one
+    start: If a programmed pose is not currently being created, start creation of one
     save: save the current pose, which includes the state of the gripper (opened or closed)
         - After saving, a numbered list of frames to save the pose in are provided. Provide
         the appropriate number to make a frame selection
@@ -24,15 +27,6 @@ Commands:
     exit: quits the program
     ***<program_name> cannot have spaces***
 """
-
-
-class Action:
-    """
-    TODO: use this class to save information about end-effector poses as part of a Program
-    """
-
-    def __init__(self):
-        pass
 
 
 class ProgramByDemoHelper:
@@ -47,29 +41,35 @@ class ProgramByDemoHelper:
     def __init__(self):
         self._ar_tags = []
         self._ar_sub = rospy.Subscriber('/ar_pose_marker', AlvarMarkers, self._ar_feedback)
-        self._controller_client = None  # TODO create action client to relax arm and call
+        self._joint_states_reader = JointStateReader()
+        self._controller_client = actionlib.SimpleActionClient('query_controller_states', QueryControllerStatesAction)
+        self._gripper = fetch_api.Gripper()
 
-    def getARTagMarkers(self):
-        return self._ar_tags
+    def get_ar_tag_markers(self):
+        return copy.deepcopy(self._ar_tags)
 
-    def getGripperPose(self):
-        pass
+    def get_default_frame(self):
+        return "base_link"
 
-    def isGripperOpen(self):
-        pass
+    def get_choose_frame_text(self):
+        choose_frame_text = "choose a frame by index:\n"
+        choose_frame_text += ("-1: %s; " % self.get_default_frame())
+        for i, tag in enumerate(self._ar_tags):
+            choose_frame_text += ('%d: %s; ' % (i, tag.id))
 
-    def isGripperClosed(self):
-        pass
+        return choose_frame_text
 
-    def runProgram(self, prog):
-        pass
-
-    #### Need a bunch of handlers for each subcriber/actionclient callback
+    # CALLBACKS
 
     def _ar_feedback(self, msg):
         self._ar_tags = msg.markers
 
-    def _send_arm_goal(self, state):
+    def _arm_feedback(self, msg):
+        self._joints = msg
+
+    # ACTIONS
+
+    def _send_arm_goal(self, controller_state):
         """
         Helper function for sending an arm goal
         :param state: RUNNING or STOPPED
@@ -77,7 +77,7 @@ class ProgramByDemoHelper:
         goal = QueryControllerStatesGoal()
         state = ControllerState()
         state.name = 'arm_controller/follow_joint_trajectory'
-        state.state = state
+        state.state = controller_state
         goal.updates.append(state)
 
         self._controller_client.send_goal(goal)
@@ -89,8 +89,11 @@ class ProgramByDemoHelper:
     def start_arm(self):
         self._send_arm_goal(ControllerState.RUNNING)
 
-    def get_action(self):
-        pass
+    def close_gripper(self):
+        gripper.close(gripper.MAX_EFFORT)
+
+    def open_gripper(self):
+        gripper.open()
 
 
 class Program:
@@ -98,22 +101,18 @@ class Program:
     This defines a program that the user can define by manually setting the end-effector positions
     and saving them with the command-line application.
     """
-
     def __init__(self):
         self._creating = False
-        self._actions = []
+        self._poses = []
 
-    def save_action(self, frame_index):
-        # TODO implement
-        self._actions.append({"action": None, "frame": FRAMES[frame_index]})
+    def save_pose(self, pose, gripper_state, frame_index):
+        self._poses.append({"pose": None, "gripper_state": gripper_state, "frame": FRAMES[frame_index]})
 
-    def get_actions(self):
-        return copy.deepcopy(self._actions)
+    def get_poses(self):
+        return copy.deepcopy(self._poses)
 
     def run(self):
-        for action in self._actions:
-            # TODO this
-            action.run()
+        pass
 
 
 MODE_MAIN = 0
@@ -121,13 +120,11 @@ MODE_PROGRAM = 1
 MODE_SELECT_FRAME = 2
 
 MODES = ["main", "program", "select_frame"]
-FRAMES = ["frame a"]  # TODO populate
 
-
-class PdbCli:
+class PbdCli:
     def __init__(self):
         self._mode = MODE_MAIN  # defines the mode of the CLI application
-        self._pdb = ProgramByDemoHelper()
+        self._pbd = ProgramByDemoHelper()
         self._current_program = None
         self._programs = {}
 
@@ -142,6 +139,7 @@ class PdbCli:
         if self._mode is MODE_MAIN:
             if command[0] == "start":
                 self._current_program = Program()
+                self._pbd.relax_arm()
                 self._mode = MODE_PROGRAM
             elif command[0] == "run":
                 if len(command) < 2:
@@ -156,11 +154,12 @@ class PdbCli:
                 print HELP_MESSAGE
         elif self._mode is MODE_PROGRAM:
             if command[0] == "save":
-                print "choose a frame by index:"
-                for i, frame in enumerate(FRAMES):
-                    sys.stdout.write('%d: %s; ' % (i, frame))
-                print ""
+                print self._pbd.get_choose_frame_text()
                 self._mode = MODE_SELECT_FRAME
+            elif command[0] == "closegripper":
+                self._pbd.close_gripper()
+            elif command[0] == "opengripper":
+                self._pbd.open_gripper()
             elif command[0] == "finish":
                 if len(command) < 2:
                     print "provide program name"
@@ -168,6 +167,7 @@ class PdbCli:
 
                 self._programs[command[1]] = self._current_program
                 self._current_program = None
+                self._pbd.start_arm()
                 self._mode = MODE_MAIN
             else:
                 print HELP_MESSAGE
@@ -177,14 +177,34 @@ class PdbCli:
             except ValueError:
                 print "provide valid index"
                 return
+            ''' Sarang: so what I've been thinking is that pbd's get action would do
+                all the heavy lifting and return a tuple that hold's the pose data
+                and the gripper open/close state respective to the frame passed in
 
-            if 0 <= index < len(FRAMES):
-                self._current_program.save_action(self._pdb.get_action(), index)
+                The save action in current program would save a tuple that consists
+                of some sort of frame identifier, the position relative to the frame,
+                and the open/close state of the gripper. Good practice in use would be
+                to save right after closing a gripper even if position doesn't change
+                so that the ordering of arm movement and gripper movement is preserved.
+
+                One thing I changed, but you can feel free to revert is that I made
+                getting the frames part of the pbd helper. This seemed to make sense because
+                we dynamically want to generate ar tag's. A potential change you might have
+                to make because of this is that you'd have to save the action in the same
+                iteration of the while loop as when you decide which frame to save in (currently
+                you do it in the next iteration when the program mode changes), just in case
+                the arm's state changes between iterations and the result from get_ar_tag_markers()
+                is different from the previous iteration's.
+            '''
+            if index == -1:
+                self._current_program.save_pose(self._pbd.get_action("base_link"))
+            elif 0 <= index < len(FRAMES):
+                self._current_program.save_pose(self._pbd.get_action())
                 self._mode = MODE_PROGRAM
             else:
                 print "illegal frame"
         else:
-            raise Exception("Illegal Pdb mode")
+            raise Exception("Illegal pbd mode")
 
     def _get_command(self):
         return raw_input('[%s] > ' % MODES[self._mode]).split(" ")
@@ -199,7 +219,7 @@ def main():
     print HELP_MESSAGE
     rospy.init_node('program_by_demo')
 
-    cli = PdbCli()
+    cli = PbdCli()
     cli.run()
 
 
--- joint_state_reader/src/joint_state_reader/reader.py
+++ joint_state_reader/src/joint_state_reader/reader.py
@@ -1,55 +1,58 @@
-#!/usr/bin/env python                                                                                  
-                                                                                                       
-import rospy
-from sensor_msgs.msg import JointState  
+#!/usr/bin/env python
 
-                                                                                        
-                                                                                                       
-class JointStateReader(object):                                                                        
-    """Listens to /joint_states and provides the latest joint angles.                                  
-                                                                                                       
-    Usage:                                                                                             
-        joint_reader = JointStateReader()                                                              
-        rospy.sleep(0.1)                                                                               
-        joint_reader.get_joint('shoulder_pan_joint')                                                   
-        joint_reader.get_joints(['shoulder_pan_joint', 'shoulder_lift_joint'])                         
-    """   
-                                                                                                
-    def __init__(self): 
-	self.jointSubscriber = rospy.Subscriber("/joint_states", JointState, self.update_data)  
-        self.jointStates = {}      
+import rospy
+from sensor_msgs.msg import JointState
+
+
+
+class JointStateReader(object):
+    """Listens to /joint_states and provides the latest joint angles.
+
+    Usage:
+        joint_reader = JointStateReader()
+        rospy.sleep(0.1)
+        joint_reader.get_joint('shoulder_pan_joint')
+        joint_reader.get_joints(['shoulder_pan_joint', 'shoulder_lift_joint'])
+    """
+
+    def __init__(self):
+        self.jointSubscriber = rospy.Subscriber("/joint_states", JointState, self.update_data)
+        self.jointStates = {}
 
     def update_data(self, message):
         for i in range(len(message.name)):
-	    self.jointStates[message.name[i]] = message.position[i]                                                                                
-                                                                                                       
-    def get_joint(self, name):                                                                         
-        """Gets the latest joint value.                                                                
-                                                                                                       
-        Args:                                                                                          
-            name: string, the name of the joint whose value we want to read.                           
-                                                                                                       
-        Returns: the joint value, or None if we do not have a value yet.                               
-        """                                                                                            
+            self.jointStates[message.name[i]] = message.position[i]
+
+    def get_joint(self, name):
+        """Gets the latest joint value.
+
+        Args:
+            name: string, the name of the joint whose value we want to read.
+
+        Returns: the joint value, or None if we do not have a value yet.
+        """
         if name in self.jointStates.keys():
-	    return self.jointStates[name]
+            return self.jointStates[name]
         else:
-	    return None                                                                                                           
-                                                                                                       
-    def get_joints(self, names):                                                                       
-        """Gets the latest values for a list of joint names.                    
-                                                                                
-        Args:                                                                   
-            name: list of strings, the names of the joints whose values we want 
-                to read.                                                        
-                                                                                
-        Returns: A list of the joint values. Values may be None if we do not    
-            have a value for that joint yet.                                    
-        """                                                                     
+            return None
+
+    def get_joints(self, names=None):
+        """Gets the latest values for a list of joint names.
+
+        Args:
+            name: list of strings, the names of the joints whose values we want
+                to read.
+
+        Returns: A list of the joint values. Values may be None if we do not
+            have a value for that joint yet.
+        """
+        if not names:
+            return copy.deepcopy(self.jointStates)
+
         result = []
         for name in names:
-	    if name in self.jointStates.keys():
-	        result.append(self.jointStates[name])
+            if name in self.jointStates.keys():
+                result.append(self.jointStates[name])
             else:
-	        result.append(None)                                         
+                result.append(None)
         return result
