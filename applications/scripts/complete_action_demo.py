cfaef45ebb8af875364efbbe681e2027877d9985
CSE 481C Team 5 <cse481c5@gmail.com>
2018-02-25 12:12:52 -0800

    made action demo but need to implement waiting for navigation to complete

--- applications/scripts/complete_action_demo.py
+++ applications/scripts/complete_action_demo.py
@@ -1,43 +1,107 @@
+#!/usr/bin/env python
+
 import rospy
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped
 import copy
 import os
 import threading
 import pickle
+import json
+from program_by_demo_demo import PbdCli, ProgramByDemoHelper
 
-# COMMANDS = """
-# start: start considering a series of actions to save
-# savegrip: save the position of the gripper
-# saveloc: save the location of the robot
-# finish <name>: finish and save the series of actions as a program
-# run <name>: run the saved program
-# list: list the names of all saved programs
-# exit: quit the program
-# help: print the commands
-# """
+MODE_MAIN = 0
+MODE_PROGRAM = 1
+MODE_SELECT_FRAME = 2
 
 MODES = ["main", "program", "select_frame"]
 COMMANDS = {
-    "main": ["start", "list", "run", "export", "import"],
-    "program": ["savegrip", "goto <N>", "listloc", "closegripper", "opengripper",
-                "len", "finish"],
+    "main": ["start", "list", "run", "export", "import", "help"],
+    "program": ["savegrip", "saveloc", "finish"],
     "select_frame": ["<N>"]
 }
 
+FIELD_GRIP_STATE = "grip_state"
 
-class ActionDemoCli(PbdCli):
+FIELD_POSITION = "position"
+
+RVIZ = True
+FILE_NAME = "program_by_demo_saved.pickle"
+DEFAULT_FRAME = "base_link"
+
+GRIPPER_OPEN = 0
+GRIPPER_CLOSE = 1
+
+def export_program(name, program):
+    pickle.dump(program, open("ad_%s.pickle" % name, "wb"))
+
+def import_program(name):
+    return pickle.load(open("ad_%s.pickle" % name, "rb"))
+
+class ActionByDemoHelper(ProgramByDemoHelper):
+    def __init__(self):
+        ProgramByDemoHelper.__init__(self)
+        self._loc_pub = rospy.Publisher("move_base_simple/goal", PoseStamped, queue_size=10)
+        self._loc_sub = rospy.Subscriber("amcl_pose", PoseWithCovarianceStamped, callback=self._loc_callback)
+        self._curr_loc = None
+
+    def _loc_callback(self, msg):
+        self._curr_loc = msg
+
+    def go_to(self, loc_msg):
+        new_msg = PoseStamped()
+        new_msg.header = loc_msg.header
+        new_msg.pose = loc_msg.pose.pose
+        self._loc_pub.publish(new_msg)
+        return True
+
+    def get_location(self):
+        return copy.deepcopy(self._curr_loc)
+
+    def run_program(self, program):
+        """
+        Runs the given program
+        :param program: list of positions
+        :return:
+        """
+        for component, pos in program:
+            if component == "arm":
+                saved_pos = pos["position"]
+                # transform back into the base frame when running
+                base_pose = self._tfl.transformPose(DEFAULT_FRAME, saved_pos)
+                self._arm.move_to_pose(base_pose)
+                if pos["grip_state"] == GRIPPER_OPEN and self._get_gripper_state() == GRIPPER_CLOSE:
+                    self._gripper.open()
+                elif pos["grip_state"] == GRIPPER_CLOSE and self._get_gripper_state() == GRIPPER_OPEN:
+                    self._gripper.close()
+            elif component == "torso":
+                self.go_to(pos)
+
+class ActionDemoCli:
     def __init__(self):
         self._mode = MODE_MAIN  # defines the mode of the CLI application
-        self._pbd = ProgramByDemoHelper()
+        self._abd = ActionByDemoHelper()
         self._current_program = []
         self._programs = {}
         self._current_ar_tags = None
-        self._map_annotator = Annotator()
 
-    def _get_pose_strings(self):
-        msgs = self._map_annotator.get_saved_msgs()
-        print msgs
-        
+    def run(self):
+        while True:
+            self._handle_command(self._get_command())
+
+    def _get_command(self):
+        return raw_input('[%s] > ' % MODES[self._mode]).split(" ")
+
+    def _print_choose_frame_text(self):
+        """
+        Prints the helper text to choose a frame
+        """
+        choose_frame_text = "choose a frame by index:\n"
+        choose_frame_text += ("-1: %s; " % DEFAULT_FRAME)
+        for i, tag in enumerate(self._current_ar_tags):
+            choose_frame_text += ('%d: %s; ' % (i, tag.id))
+
+        print choose_frame_text
+
     def _handle_command(self, command):
         if command[0] == "exit":
             exit(0)
@@ -54,7 +118,7 @@ class ActionDemoCli(PbdCli):
                     return False
                 return True
             if command[0] == "start":
-                self._pbd.relax_arm()
+                self._abd.relax_arm()
                 self._mode = MODE_PROGRAM
             elif command[0] == "list":
                 for name in self._programs:
@@ -63,7 +127,7 @@ class ActionDemoCli(PbdCli):
                 if not two_args(command):
                     return
                 print "running program %s" % command[1]
-                self._pbd.run_program(self._programs[command[1]])
+                self._abd.run_program(self._programs[command[1]])
             elif command[0] == "export":
                 if not two_args(command):
                     return
@@ -75,37 +139,18 @@ class ActionDemoCli(PbdCli):
                     print "provide program name"
                     return
                 self._programs[command[1]] = import_program(command[1])
+            elif command[0] == "help":
+                json.dumps(COMMANDS)
             else:
                 print "bad command"
         elif self._mode is MODE_PROGRAM:
-            def two_args(command):
-                if len(command) < 2:
-                    print "provide goto location"
-                    return False
-                if command[1] not in self._get_pose_strings: # todo: change self._locations to be a list of location strings
-                    print "invalid location"
-                    return False
-                return True
             if command[0] == "savegrip":
                 # Save the current ar tags so that there's no inconsistency in the tag
-                self._current_ar_tags = self._pbd.get_ar_tag_markers()
+                self._current_ar_tags = self._abd.get_ar_tag_markers()
                 self._print_choose_frame_text()
                 self._mode = MODE_SELECT_FRAME
-            elif command[0] == "goto":
-                if not two_args(command):
-                    return
-                print "moving to location %s" % command[1]
-                # todo add a goto thing
-                pass
-            elif command[0] == "listloc":
-                for name in self._get_pose_strings: # todo: change self._locations to be a list of locations
-                    print name
-            elif command[0] == "closegripper":
-                self._pbd.close_gripper()
-            elif command[0] == "opengripper":
-                self._pbd.open_gripper()
-            elif command[0] == "len":
-                print len(self._current_program)
+            elif command[0] == "saveloc":
+                self._current_program.append(("torso", self._abd.get_location()))
             elif command[0] == "finish":
                 if len(command) < 2:
                     print "provide program name"
@@ -113,7 +158,7 @@ class ActionDemoCli(PbdCli):
 
                 self._programs[command[1]] = self._current_program
                 self._current_program = []
-                self._pbd.start_arm()
+                self._abd.start_arm()
                 self._mode = MODE_MAIN
             else:
                 print "bad command"
@@ -129,12 +174,12 @@ class ActionDemoCli(PbdCli):
                 frame_string = DEFAULT_FRAME
                 if index >= 0:
                     frame_string = "ar_marker_%d" % self._current_ar_tags[index].id
-                self._current_program.append(self._pbd.get_position(frame_string))
+                self._current_program.append(("arm", self._abd.get_position(frame_string)))
                 self._mode = MODE_PROGRAM
             else:
                 print "illegal frame"
         else:
-            raise Exception("Illegal pbd mode")
+            raise Exception("Illegal abd mode")
 
 
 def wait_for_time():
@@ -144,7 +189,7 @@ def wait_for_time():
 
 def main():
     print json.dumps(COMMANDS)
-    rospy.init_node('program_by_demo')
+    rospy.init_node('action_by_demo')
 
     cli = ActionDemoCli()
     cli.run()
